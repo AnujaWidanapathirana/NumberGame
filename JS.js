9eaf70d61bdf4a2c62019ec2fb878dd0e2153a69
AnujaWidanapathirana <anujawidanapathirana@gmail.com>
2024-06-26 15:41:18 +0530

    main

--- JS.js
+++ JS.js
@@ -5,12 +5,16 @@ function checkGuess() {
     const userGuess=document.getElementById('guess').value;
     const message=document.getElementById('message');
     attempts++;
+    if(attempts == 20){
+        message.textContent = `You weren't able to find the number in 20 attempts.You Have A whole one cell in your brain.:) just give up`;
+        message.style.color = 'red';
+    }
     l1:if(userGuess==randomNumber) {
         message.textContent = `Congratulations! You guessed the number in ${attempts} attempts.`;
         message.style.color = 'green';
         if(attempts==1){
-            message.textContent = `You are the luckiest person Alive.Buy A lottery on your way home.`;
-            message.style.color = 'green';
+            message2.textContent = `You are the luckiest person Alive.Buy A lottery on your way home.`;
+            message2.style.color = 'green';
         }
     }else if (userGuess>randomNumber) {
         if(userGuess-10>randomNumber){
@@ -18,7 +22,7 @@ function checkGuess() {
         message.style.color='red';
         break l1; 
         }
-        message.textContent='Closer than Ever...A little Higer! Try again.';
+        message.textContent='Closer than Ever...A little Lower! Try again.';
         message.style.color='orange';
     }else if(userGuess<randomNumber){
         if(userGuess+10>randomNumber){
@@ -26,11 +30,10 @@ function checkGuess() {
             message.style.color='red';
             break l1; 
             }
-        message.textContent='Closer than Ever...A little lower! Try again.';
+        message.textContent='Closer than Ever...A little Higer! Try again.';
         message.style.color='orange';
-    }
+        }
 }
-
 
 function resetGame() {
     randomNumber=Math.floor(Math.random()*100)+1;
